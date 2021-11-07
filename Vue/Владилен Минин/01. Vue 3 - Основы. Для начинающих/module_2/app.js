6f0661f8b0bb764b10f767d4c64031c23b22e412
Evgeny <44028525+logdog90@users.noreply.github.com>
2021-11-07 23:40:47 +1000

    Модуль 2 завершён!

--- Vue/Владилен Минин/01. Vue 3 - Основы. Для начинающих/module_2/app.js
+++ Vue/Владилен Минин/01. Vue 3 - Основы. Для начинающих/module_2/app.js
@@ -1,5 +1,33 @@
 Vue.createApp({
     data: () => ({
-        
-    })
+        myHtml: '<h1>Vue 3 App</h1>',
+        title: 'Я есть Грут',
+        person: {
+            firstName: 'Johnny',
+            lastName: 'Bashlak',
+            age: 31
+        },
+        items: [1, 2]
+    }),
+
+    methods: {
+        addItem() {
+            this.items.unshift(this.$refs.myInput.value)
+            this.$refs.myInput.value = ''
+        },
+
+        remove(i) {
+            this.items.splice(i, 1)
+        },
+
+        log(item) {
+            console.log('Log Item: ', item)
+        }
+    },
+
+    computed: {
+        evenItems() {
+            return this.items.filter(i => i % 2 === 0)
+        }
+    }
 }).mount('#app')
