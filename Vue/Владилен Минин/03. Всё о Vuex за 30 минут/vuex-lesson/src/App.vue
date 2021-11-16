e288e1bcd10ef0a37845ed03bf4676eb0f6d6505
Evgeny <44028525+logdog90@users.noreply.github.com>
2021-11-16 20:53:51 +1000

    Подключил Vuex

--- Vue/Владилен Минин/03. Всё о Vuex за 30 минут/vuex-lesson/src/App.vue
+++ Vue/Владилен Минин/03. Всё о Vuex за 30 минут/vuex-lesson/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="post" v-for="post in posts" :key="post.id">
+    <div class="post" v-for="post in allPosts" :key="post.id">
       <h2>{{ post.title }}</h2>
       <p>{{ post.body }}</p>
     </div>
@@ -12,9 +12,9 @@
 
 export default {
   name: 'App',
-  data() {
-    return {
-      posts: []
+  computed: {
+    allPosts() {
+      return this.$store.getters.allPosts
     }
   },
   async mounted() {
