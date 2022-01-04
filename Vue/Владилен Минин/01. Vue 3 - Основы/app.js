6e38893c39642ae1772cc9cacd5fa6350e5f99b4
Evgeny <44028525+logdog90@users.noreply.github.com>
2022-01-04 21:06:52 +1000

    VueJS - Основы. Добавление элементов при помощи модификатора

--- Vue/Владилен Минин/01. Vue 3 - Основы/app.js
+++ Vue/Владилен Минин/01. Vue 3 - Основы/app.js
@@ -11,11 +11,6 @@ const App = {
         addNewNote() {
             this.notes.push(this.inputValue)
             this.inputValue = ''
-        },
-        inputKeyPress(event) {
-            if (event.key === 'Enter') {
-                this.addNewNote()
-            }
         }
     }
 }
