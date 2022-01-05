68cc159609af57e9795c08cdf6c6672b36432a33
Evgeny <44028525+logdog90@users.noreply.github.com>
2022-01-05 18:54:51 +1000

    VueJS - Основы. Часть 2 - Модификатор @click.stop

--- Vue/Владилен Минин/02. Vue 3 - Основы. Часть 2/app.js
+++ Vue/Владилен Минин/02. Vue 3 - Основы. Часть 2/app.js
@@ -8,10 +8,5 @@ Vue.createApp({
       age: 31
     },
     items: [1, 2, 3, 4, 5, 6]
-  }),
-  methods: {
-    stopPropagation(event) {
-      event.stopPropagation()
-    }
-  }
+  })
 }).mount('#app')
