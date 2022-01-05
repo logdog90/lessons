b75b21c5eeb3283b597f79c4f6b303688369756b
Evgeny <44028525+logdog90@users.noreply.github.com>
2022-01-05 18:26:30 +1000

    VueJS - Основы. Часть 2 - Цыклы с v-for

--- Vue/Владилен Минин/02. Vue 3 - Основы. Часть 2/app.js
+++ Vue/Владилен Минин/02. Vue 3 - Основы. Часть 2/app.js
@@ -1,6 +1,12 @@
 Vue.createApp({
   data: () => ({
     myHtml: '<h1>Заголовок первого уровня<h1/>',
-    title: 'Заголовок второго уровня'
+    title: 'Заголовок второго уровня',
+    person: {
+      firstName: 'Johnny',
+      lastName: 'Bashlak',
+      age: 31
+    },
+    items: [1, 2, 3, 4, 5, 6]
   })
 }).mount('#app')
