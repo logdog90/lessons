d0f2fc0c6f80db3d6180a8009c781e1a1b4617bd
Evgeny <44028525+logdog90@users.noreply.github.com>
2022-01-05 18:52:08 +1000

    VueJS - Основы. Часть 2 - Метод stopPropagation()

--- Vue/Владилен Минин/02. Vue 3 - Основы. Часть 2/app.js
+++ Vue/Владилен Минин/02. Vue 3 - Основы. Часть 2/app.js
@@ -8,5 +8,10 @@ Vue.createApp({
       age: 31
     },
     items: [1, 2, 3, 4, 5, 6]
-  })
+  }),
+  methods: {
+    stopPropagation(event) {
+      event.stopPropagation()
+    }
+  }
 }).mount('#app')
