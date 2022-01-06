ddf64be731fb6e6d94e03b989b58d093f3d0e840
Evgeny <44028525+logdog90@users.noreply.github.com>
2022-01-06 14:41:11 +1000

    Как работает Vue - Реактивность и прокси

--- Vue/Владилен Минин/03. Как работает Vue/app.js
+++ Vue/Владилен Минин/03. Как работает Vue/app.js
@@ -1,6 +1,16 @@
-let message = 'серенький козлик'
-let title = "Жил-был у бабушке " + message
+const data = {
+	message: "серенький козлик",
+	title: "Жил-был у бабушке серенький козлик",
+}
 
-console.log(title)
-message = 'серенький ослик'
-console.log(title);
+const proxy = new Proxy(data, {
+	set(target, key, value) {
+		if (key === 'message') {
+			target.title = 'Жил-был у бабушке ' + value
+		}
+		target[key] = value
+	}
+})
+
+proxy.message = 'серенький ослик'
+console.log(proxy)
