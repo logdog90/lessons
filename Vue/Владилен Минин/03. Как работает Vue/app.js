ab82cc6fbbbdec7c97571657baff7320d3ff95cd
Evgeny <44028525+logdog90@users.noreply.github.com>
2022-01-06 14:14:01 +1000

    Как работает Vue - JavaScript не реактивный!

--- Vue/Владилен Минин/03. Как работает Vue/app.js
+++ Vue/Владилен Минин/03. Как работает Vue/app.js
@@ -1,35 +1,6 @@
-const h = Vue.h
+let message = 'серенький козлик'
+let title = "Жил-был у бабушке " + message
 
-const app = Vue.createApp({
-	data: () => ({
-		title: "Это из свойства template",
-	}),
-
-	methods: {
-		changeTitle() {
-			this.title = 'Изменили!'
-		}
-	},
-
-	render() {
-		return h(
-			"div",
-			{
-				class: "card center",
-			},
-			[
-				h("h1", {}, this.title),
-				h(
-					"button",
-					{
-						class: "btn",
-						onClick: this.changeTitle,
-					},
-					"Изменить"
-				),
-			]
-		);
-	},
-});
-
-app.mount("#app");
+console.log(title)
+message = 'серенький ослик'
+console.log(title);
